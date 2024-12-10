32892c104932719a8a7deb1a58535b677cc70144
vokske <kevinmuigai2@gmail.com>
2024-12-10 23:17:42 +0300

    Added attributes for table creation in sqlalchemy

--- models/city.py
+++ models/city.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class City(BaseModel, Base):
+    """ The city class that represents a city in the HBNB project."""
+    __tablename__ = "cities"
+    
+    if isinstance(Base, type):
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="all, delete, delete-orphan")
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    else:
+        name = ""
+        state_id = ""
