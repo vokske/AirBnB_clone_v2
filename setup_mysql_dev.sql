ee68ffb0b8e5ba4f373a73dce52fdf09dc3f7569
vokske <kevinmuigai2@gmail.com>
2024-12-10 23:37:36 +0300

    SQL scripts for preparing a MYSQL server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,13 +1,9 @@
 -- Script prepares a MYSQL server for the project
-
 -- First, create a database
-CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
 -- Create a new user in localhost
-CREATE USER IF NOT EXISTS hbnb_dev@localhost IDENTIFIED BY 'hbnb_dev_db';
-
---Grant user all privileges on selected database
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
+-- Grant user all privileges on selected database
 -- Grant user SELECT privilege on 'performance_schema' db
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnbdevpwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
