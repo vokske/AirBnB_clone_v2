5a3c50fc1dbbad80081f1cff0e90ec7322e6dba6
vokske <kevinmuigai2@gmail.com>
2024-12-10 23:23:56 +0300

    Modified class by adding sqlalchemy functionality

--- models/review.py
+++ models/review.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
 
+from os import getenv
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.base_model import BaseModel, Base
+else:
+    from models.base_model import BaseModel
+    Base = object
+
+class Review(BaseModel, Base):
+    """ Review class to store review information """
+    __tablename__ = "reviews"
+
+    if isinstance(Base, type):
+        from sqlalchemy import Column, String, ForeignKey
+
+        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
