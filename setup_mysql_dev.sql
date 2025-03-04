b526c2cb7e9696b2601aa4d7b17c75b3532a967e
vokske <kevinmuigai2@gmail.com>
2025-03-04 11:22:43 +0300

    MySQL setup development

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,6 +4,6 @@
 -- Grant user all privileges on selected database
 -- Grant user SELECT privilege on 'performance_schema' db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnbdevpwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
