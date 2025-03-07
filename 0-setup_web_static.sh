600cdccfbb3e91bbe0c245613fde9bf9221e07bc
vokske <kevinmuigai2@gmail.com>
2025-03-07 17:49:36 +0300

    Prepare web servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,35 +3,35 @@
 
 sudo apt-get upgrade
 sudo apt-get -y install nginx
-mkdir -p /data/
-mkdir -p /data/web_static/
-mkdir -p /data/web_static/releases/
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
-echo "This is some test text" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -R ubuntu:ubuntu /data/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "This is some test text" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
 sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
 server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-	root /var/www/html;
-	index index.html;
+        root /var/www/html;
+        index index.html;
 
-	server_name _;
+        server_name _;
 
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
+        location / {
+                try_files \$uri \$uri/ =404;
+        }
 
-	location /redirect_me {
-		return 301 https://www.google.com/;
-	}
-	
-	location /hbnb_static {
-		alias /data/web_static/current/
-	}
+        location /redirect_me {
+                return 301 https://www.google.com/;
+        }
+        
+        location /hbnb_static {
+                alias /data/web_static/current/;
+        }
 }
 EOF
 
