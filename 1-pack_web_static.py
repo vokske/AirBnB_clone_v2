5439d27c24f0de2c08756be6b531cda8f390c30a
vokske <kevinmuigai2@gmail.com>
2025-03-15 11:44:56 +0300

    Compress files and create archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""
+This script is meant to generate a .gtz archive from the contents of the web_static folder from the Airbnb clone repo, using a function do_pack.
 
+The script imports the task function from Fabric, the datetime module from datetime, os module, and the tarfile module.
+"""
 from fabric import task
 from datetime import datetime
 import os
@@ -7,7 +11,7 @@ import tarfile
 
 @task
 def do_pack(c):
-    """A function that generates a .tgz archive from the contents of a specific folder."""
+    """A function that generates a .tgz archive from the contents of a specific folder """
     try:
         # Create versions folder if it doesn't exist
         if not os.path.exists('versions'):
