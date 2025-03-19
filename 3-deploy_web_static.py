3c2a39266b746ca08540815d853056751a916414
vokske <kevinmuigai2@gmail.com>
2025-03-19 07:40:27 -0700

    Full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,7 +4,7 @@ This script is meant to generate a .gtz archive from the contents of the web_sta
 
 The script imports the task function from Fabric, the datetime module from datetime, os module, and the tarfile module.
 """
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
 from datetime import datetime
 import os
 import tarfile
@@ -30,7 +30,6 @@ def do_pack():
         archive_name = f'versions/web_static_{datetime.now().strftime("%Y%m%d%H%M%S")}.tgz'
 
         # Create a .tgz archive using tarfile
-
         with tarfile.open(archive_name, 'w:gz') as tar:
             tar.add("web_static", arcname=os.path.basename("web_static"))
 
